2147fb221127dfe6df2443489e83bc4a18c13c36
Carlos Peralta <carlos.peralta@globallogic.com>
2015-11-16 14:09:00 -0300

    Initial Commit

---
+++ RedditTop50/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditTop50
+//
+//  Created by Carlos Peralta on 16/11/15.
+//  Copyright © 2015 Carlos Peralta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedditTop50/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedditTop50
+//
+//  Created by Carlos Peralta on 16/11/15.
+//  Copyright © 2015 Carlos Peralta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
